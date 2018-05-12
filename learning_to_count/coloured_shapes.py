2c3b8d0146d194b9aa8708cab4f3f3fdb4f1eee8
jmsinusa <simonbolivar.uk@gmail.com>
2018-05-12 11:42:23 -0400

    First working version

--- learning_to_count/coloured_shapes.py
+++ learning_to_count/coloured_shapes.py
@@ -10,32 +10,84 @@ from __future__ import print_function
 
 import numpy as np
 from matplotlib import pyplot as plt
-from keras.utils import to_categorical, plot_model
-from keras.optimizers import Adam,SGD
-from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
-from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
-from keras.models import Model
-from keras import backend as K
-from keras.callbacks import ModelCheckpoint
-
-from keras.models import Sequential
+import os
+import pickle
+# from keras.utils import to_categorical, plot_model
+# from keras.optimizers import Adam,SGD
+# from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+# from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+# from keras.models import Model
+# from keras import backend as K
+# from keras.callbacks import ModelCheckpoint
+#
+# from keras.models import Sequential
 
 from data_creator import DataCreator
 
 
-def create_trinagles():
+def create_square(n_forlabel, save_filename, noise_scale=30,
+                  labelcolour=(230, 30, 30), labelsize=(3, 3), bkgdsize=(90, 90, 3)):
     '''Description'''
     dd = DataCreator(dtype=np.uint8)
-    dd.zeros_bkgd((100, 100, 3))
+    dd.zeros_bkgd(bkgdsize)
     # dd.add_noise(scale=10.0, abs_noise=True)
-    for ii in range(10):
-        error = dd.add_shape(shape='square', colour=(255, 255, 255), size=(3, 3))
-        error = dd.add_shape(shape='square', colour=(255, 0, 0), size=(3, 3))
-        error = dd.add_shape(shape='square', colour=(10, 0, 255), size=(4, 1))
-    dd.add_noise(scale=40.0, abs_noise=True)
-    plt.imshow(dd.img)
-    plt.show()
+    errors = 0
+    n_forlabel = n_forlabel
+    for ii in range(n_forlabel):
+        # errors += dd.add_shape(shape='square', colour=(255, 255, 255), size=(3, 3))
+        errors += dd.add_shape(shape='square', colour=labelcolour, size=labelsize)
+        # error = dd.add_shape(shape='square', colour=(10, 0, 255), size=(4, 1))
 
+    dd.add_noise(scale=noise_scale, noise='uniform', abs_noise=True)
+    # plt.imshow(dd.img)
+    # plt.show()
+    n_forlabel -= errors
+    plt.imsave(arr=dd.img, fname=save_filename, format='png')
+    return n_forlabel
+
+
+def create_ex1(n_examples, save_dir):
+    labels = {}
+    n_forlabels = np.random.randint(0, high=50, size=n_examples)
+    for eg_no in range(n_examples):
+        save_filename = "sample%05i.png" % eg_no
+        save_filepath = os.path.join(save_dir, save_filename)
+        n_forlabel = n_forlabels[eg_no]
+        n_label = create_square(n_forlabel, save_filepath)
+        labels[save_filename] = n_label
+    pickle.dump(labels, open(os.path.join(save_dir, 'labels.pkl'), "wb"))
+
+
+def create_npy_dataset(dir_):
+    assert os.path.isdir(dir_)
+    pickle_file = os.path.join(dir_, 'labels.pkl')
+    assert os.path.isfile(pickle_file)
+    labels = pickle.load(open(pickle_file, "rb"))
+    list_files_raw = os.listdir(dir_)
+    png_list = []
+    for ff in list_files_raw:
+        if os.path.splitext(ff)[1] in ['.png']:
+            png_list.append(ff)
+    print("%i PNGs in directory." % len(png_list))
+    # Load first img
+    img0 = plt.imread(os.path.join(dir_, png_list[0]))
+    img_shape_xy = img0.shape[:2]
+    data_npy = np.zeros((len(png_list), img_shape_xy[0], img_shape_xy[1], 3), dtype=np.float32)
+    labels_npy = np.zeros(len(png_list), dtype=np.uint8)
+    for ii, png_ff in enumerate(png_list):
+        img = plt.imread(os.path.join(dir_, png_ff))
+        label = labels[png_ff]
+        data_npy[ii, :, :, :] = img[:, :, :3]
+        labels_npy[ii] = label
+    np.save(os.path.join(dir_, 'data.npy'), data_npy)
+    np.save(os.path.join(dir_, 'labels.npy'), labels_npy)
 
 if __name__ == "__main__":
-    create_trinagles()
+    dir_ = r'/Users/james/blog/20180512-ColouredShapeCounting/ex1'
+    # create_ex1(10, r'/Users/james/blog/20180512-ColouredShapeCounting/ex1')
+    create_npy_dataset(dir_)
+    # data = np.load(os.path.join(dir_, 'data.npy'))
+    # labels = np.load(os.path.join(dir_, 'labels.npy'))
+    # print(labels[2])
+    # plt.imshow(data[2, :, :, :])
+    # plt.show()
--- learning_to_count/data_creator.py
+++ learning_to_count/data_creator.py
@@ -15,6 +15,7 @@ class DataCreator(object):
     """
     Generic data creation class
     """
+
     def __init__(self, dtype=np.uint8):
         """
         None
@@ -22,7 +23,6 @@ class DataCreator(object):
         self.img = None
         self.dtype = dtype
         self.mask = None
-
 
     def zeros_bkgd(self, size):
         """
@@ -33,7 +33,6 @@ class DataCreator(object):
         """
         self.img = np.zeros(size, dtype=self.dtype)
         self.mask = np.zeros((size[0], size[1]), dtype=np.bool)
-
 
     def add_noise(self, noise='normal', mean=0, scale=1.0, abs_noise=True):
         """
@@ -47,8 +46,11 @@ class DataCreator(object):
         """
         if noise == 'normal':
             noise_array = np.random.normal(loc=mean, scale=scale, size=self.img.shape)
+        elif noise == 'uniform':
+            noise_array = np.random.uniform(low=int(mean - scale), high=int(mean + scale),
+                                            size=self.img.shape)
         else:
-            raise NotImplementedError('Noise type "%s" not recognised.'% noise)
+            raise NotImplementedError('Noise type "%s" not recognised.' % noise)
         if abs_noise:
             noise_array = np.abs(noise_array, out=noise_array)
         out = self.img + noise_array
@@ -56,14 +58,17 @@ class DataCreator(object):
         out[out < 0] = 0
         self.img = out.astype(self.dtype)
 
-    def add_shape(self, shape = 'square', colour=(255, 255, 255), size = (3, 3)):
+    def add_shape(self, shape='square', colour=(255, 255, 255), size=(3, 3)):
         """Add shape to random location in self.img, unless self.mask = True for any pixel."""
         (maxy, maxx, bands) = self.img.shape
         assert len(colour) == bands
         loc = None
-        size_to_check_y = int(np.ceil((size[0] - 1.0) / 2.0))
-        size_to_check_x = int(np.ceil((size[1] - 1.0) /2.0))
-        coords = []
+        size_to_check_y = int(np.ceil((size[0] - 1.0) / 2.0)) + 1
+        size_to_check_x = int(np.ceil((size[1] - 1.0) / 2.0)) + 1
+        size_to_colour_y = int(np.ceil((size[0] - 1.0) / 2.0))
+        size_to_colour_x = int(np.ceil((size[1] - 1.0) / 2.0))
+        check_coords = []
+        colour_coords = []
         attempts = 0
         while not loc:
             loc_y = np.random.randint(size_to_check_y, maxy - size_to_check_y)
@@ -72,27 +77,31 @@ class DataCreator(object):
                 test_loc_y = loc_y + ss
                 for tt in range(-size_to_check_x, size_to_check_x + 1):
                     test_loc_x = loc_x + tt
-                    coords.append((test_loc_x, test_loc_y))
+                    check_coords.append((test_loc_x, test_loc_y))
+            for ss in range(-size_to_colour_y, size_to_colour_y + 1):
+                test_loc_y = loc_y + ss
+                for tt in range(-size_to_colour_x, size_to_colour_x + 1):
+                    test_loc_x = loc_x + tt
+                    colour_coords.append((test_loc_x, test_loc_y))
+
             all_false = True
-            for cc_x, cc_y in coords:
+            for cc_x, cc_y in check_coords:
                 if self.mask[cc_y, cc_x]:
                     all_false = False
             if all_false:
                 # Set mask to True and replace self.img with colour
-                for cc_x, cc_y in coords:
+                for cc_x, cc_y in check_coords:
                     self.mask[cc_y, cc_x] = True
+                for cc_x, cc_y in colour_coords:
                     self.img[cc_y, cc_x, :] = colour
                 loc = True
             else:
                 attempts += 1
-                if attempts > 1000:
+                if attempts > 500:
                     loc = True
                     print('Warning: Cannot find space for a new shape.')
                     return True
         return False
-
-
-
 
 
 if __name__ == "__main__":
